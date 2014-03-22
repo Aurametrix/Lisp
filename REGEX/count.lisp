9361a0d934f11934e8603dfbcb3007599cb02aa8
Irene <irene@aurametrix.com>
2014-03-21 17:20:28 -0700

    word and character count

---
+++ REGEX/count.lisp
@@ -0,0 +1,15 @@
+(defun my-count-words-region (posBegin posEnd)
+  "Count number of words and characters"
+  (interactive "r")
+  (message "Counting …")
+  (save-excursion
+    (let (wordCount charCount)
+      (setq wordCount 0)
+      (setq charCount (- posEnd posBegin))
+      (goto-char posBegin)
+      (while (and (< (point) posEnd)
+                  (re-search-forward "\\w+\\W*" posEnd t))
+        (setq wordCount (1+ wordCount)))
+
+      (message "Words: %d. Chars: %d." wordCount charCount)
+      )))
