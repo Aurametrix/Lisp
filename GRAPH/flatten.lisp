6bc01a2ac59b8a6f48da38b1b7277e46d3fa1ddc
Irene <irene@aurametrix.com>
2015-05-01 20:40:50 -0700

    faster recursive version

--- GRAPH/flatten.lisp
+++ GRAPH/flatten.lisp
@@ -2,3 +2,13 @@
   (cond ((null structure) nil)
         ((atom structure) (list structure))
         (t (mapcan #'flatten structure))))
+
+(defun flatten (obj)
+  (let (result)
+    (labels ((grep (obj)
+               (cond ((null obj) nil)
+                     ((atom obj) (push obj result))
+                     (t (grep (rest obj))
+                        (grep (first obj))))))
+      (grep obj)
+      result)))
