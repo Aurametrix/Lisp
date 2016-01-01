19d3a055ed9485f7e940033dd608881871195603
Aurametrix <irene@aurametrix.com>
2015-12-31 21:35:17 -0800

    iterative version

--- MATH/Counting-coins.lisp
+++ MATH/Counting-coins.lisp
@@ -1,18 +1,6 @@
-(defun count-change (amount coins
-                    &optional
-                    (length (1- (length coins)))
-                    (cache  (make-array (list (1+ amount) (length coins))
-                                        :initial-element nil)))
-  (cond ((< length 0) 0)
-        ((< amount 0) 0)
-        ((= amount 0) 1)
-        (t (or (aref cache amount length)
-               (setf (aref cache amount length)
-                     (+ (count-change (- amount (first coins)) coins length cache)
-                        (count-change amount (rest coins) (1- length) cache)))))))
- 
-; (compile 'count-change) ; for CLISP
- 
-(print (count-change 100 '(25 10 5 1)))		   ; = 242
-(print (count-change 100000 '(100 50 25 10 5 1)))  ; = 13398445413854501
-(terpri)
+(defun count-change (amount coins &aux (ways (make-array (1+ amount) :initial-element 0)))
+  (setf (aref ways 0) 1)
+  (loop for coin in coins do
+        (loop for j from coin upto amount
+              do (incf (aref ways j) (aref ways (- j coin)))))
+  (aref ways amount))
