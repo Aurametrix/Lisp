f852150e9b9a8d4ba6c182caf2f0b162f750db28
Aurametrix <irene@aurametrix.com>
2016-01-06 19:44:06 -0800

    Memory as Vectors

---
+++ DATA-STRUCTURES/COLLECTIONS/memory.lisp
@@ -0,0 +1,7 @@
+;;; memory operations can be described with two primitive Scheme procedures for manipulating vectors:
+
+;;; returns the nth element of the vector.
+(vector-ref <vector> <n>) 
+
+;;; sets the nth element of the vector to the designated value.
+(vector-set! <vector> <n> <value>) 
