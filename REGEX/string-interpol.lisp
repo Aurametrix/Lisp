862486349f4b59ad69af45452d8dd4931ccf3ffe
Aurametrix <irene@aurametrix.com>
2017-04-20 21:43:59 -0700

    native

--- REGEX/string-interpol.lisp
+++ REGEX/string-interpol.lisp
@@ -2,3 +2,15 @@
 
 "It's a lovely ~day morning" ;=> "It's a lovely Sunday morning"
 "(+ 1 1) is ~(+ 1 1)" ;=> "(+ 1 1) is 2"
+
+
+primitives :: [(String, [LispVal] -> LispVal)]
+primitives = [ -- ...
+              ("string-append", stringAppend)]
+
+stringAppend =
+  String . concatMap unpackString
+
+unpackString (String s) = s
+unpackString v = show v
+
