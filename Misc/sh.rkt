8c9551b474ac3b4a70e809e26010120c44103d93
Aurametrix <irene@aurametrix.com>
2018-04-19 18:43:29 -0700

    shell
    https://github.com/tonyg/racket-something/blob/master/examples/sh.rkt
    https://news.ycombinator.com/item?id=16878498

---
+++ Misc/sh.rkt
@@ -0,0 +1,21 @@
+#lang something/shell
+  // Simple demos
+
+  ls -la $HOME | grep "^d" | fgrep -v "."
+
+  ls -la | wc -l | read-line |> string-split |> car |> string->number |> \
+    printf "There are ~a lines here." | sed -e "s: are : seem to be :"
+  (newline)
+
+  def ps-output
+    pipeline
+      ps -wwwax
+      preserve-header 1 {: grep "racket" }
+      space-separated-columns [string->number]
+      |> csv-expr->table
+  print ps-output
+
+  def message-box text:
+    whiptail --title "Testing" --ok-button "OK" --msgbox text 8 50
+
+  message-box "This is pretty cool."
